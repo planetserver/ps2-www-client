e9d6421349cacb6b9c98e2f3c7d2195ac868b6b6
Tsvetan Rankov <t.rankov@jacobs-university.de>
2016-03-15 10:38:07 +0100

    All Footprints visualized on Mars

--- html/landing.js
+++ html/landing.js
@@ -100,11 +100,6 @@ function (ww,
     boundary4.push(new WorldWind.Location(	-47.727	,	4.704	));
     boundary4.push(new WorldWind.Location(	-47.763	,	4.714	));
 
-
-
-
-
-
     // Create and set attributes for it. The shapes below except the surface polyline use this same attributes
     // object. Real apps typically create new attributes objects for each shape unless they know the attributes
     // can be shared among shapes.
@@ -113,7 +108,6 @@ function (ww,
     attributes.drawInterior = false;
     var shape = new WorldWind.SurfacePolygon(boundary, attributes);
     shapesLayer.addRenderable(shape);
-
     var shape2 = new WorldWind.SurfacePolygon(boundary2, attributes);
     shapesLayer.addRenderable(shape2);
     var shape3 = new WorldWind.SurfacePolygon(boundary3, attributes);
@@ -124,6 +118,22 @@ function (ww,
     var surfaceImage2 = new WorldWind.SurfaceImage(new WorldWind.Sector(-47.57565, -47.36640, 4.220789, 4.535433),
     "http://212.201.45.9:8080/rasdaman/ows?query=for%20data%20in%20(%20frt00003590_07_if164l_trr3%20)%20return%20encode(%20{%20red:%20(int)(255%20/%20(max((data.band_233%20!=%2065535)%20*%20data.band_233)%20-%20min(data.band_233)))%20*%20(data.band_233%20-%20min(data.band_233));%20green:%20(int)(255%20/%20(max((data.band_78%20!=%2065535)%20*%20data.band_78)%20-%20min(data.band_78)))%20*%20(data.band_78%20-%20min(data.band_78));%20blue:(int)(255%20/%20(max((data.band_13%20!=%2065535)%20*%20data.band_13)%20-%20min(data.band_13)))%20*%20(data.band_13%20-%20min(data.band_13));%20alpha:%20(data.band_100%20!=%2065535)%20*%20255%20},%20%22png%22,%20%22nodata=null%22)");
 
+
+    //Adding footprints
+    var boundaries = []; // array for boundary locations of the footprints
+    var shapes = []; // array for shape SurfacePolygon objects with corresponding boundaries and attributes
+    for(var i = 0; i < Footprints.length; i++) {
+        boundaries.push([]); // new array for the boundaries of a single footprint
+        shapes.push([]);
+        for(var j = 0; j < Footprints[i].latitude.length; j++) { 
+            // adding all the boundaries of a single polygon into boundaries[i]           
+            boundaries[i].push(new WorldWind.Location(Footprints[i].latitude[j], Footprints[i].longitude[j]));
+        }
+        shapes[i] = new WorldWind.SurfacePolygon(boundaries[i], attributes);
+        shapesLayer.addRenderable(shapes[i]);
+     }
+
+
     // Add the surface images to a layer and the layer to the World Window's layer list.
     var surfaceImageLayer = new WorldWind.RenderableLayer();
     surfaceImageLayer.displayName = "Surface Images";
@@ -131,6 +141,9 @@ function (ww,
     wwd.addLayer(surfaceImageLayer);
     shapesLayer.addRenderable(surfaceImageLayer);
     //surface image test end
+
+
+
 
     // Create a layer manager for controlling layer visibility.
     var layerManger = new LayerManager(wwd);
