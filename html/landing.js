b99bd89353f9f48d3289d67bfa30d37b3faa8014
Tsvetan Rankov <t.rankov@jacobs-university.de>
2016-03-16 17:54:31 +0100

    New graph implementation with D3 only (needs some fixes)

--- html/landing.js
+++ html/landing.js
@@ -76,7 +76,6 @@ wwd.redraw();
         shapesLayer.addRenderable(shapes[i]);
      }
 
-     console.log("daskldklashs");
     // Add the surface images to a layer and the layer to the World Window's layer list.
     var surfaceImageLayer = new WorldWind.RenderableLayer();
     surfaceImageLayer.displayName = "Surface Images";
@@ -128,6 +127,18 @@ getQueryResponseAndSetChart(query);
 
             };
 
+// adjusting the data we have for plotting with D3 library
+function adjustData(parsedFloats){
+  
+  var Xaxis = [];
+  for(var i = 0; i < parsedFloats.length; i++){
+    Xaxis.push(i);
+  }
+  var XandYaxis = [];
+  XandYaxis.push(Xaxis);
+  XandYaxis.push(parsedFloats);
+  return XandYaxis;
+}
 
             function getQueryResponseAndSetChart(query) {
     var rawFile = new XMLHttpRequest();
@@ -138,7 +149,8 @@ getQueryResponseAndSetChart(query);
                 serverResponse = rawFile.responseText;
                 var parsedFloats = [];
                 parsedFloats = parseFloats(serverResponse);
-                loadScriptAndCall("//d3js.org/d3.v3.min.js", implementChart(parsedFloats));
+                
+                loadScriptAndCall("//d3js.org/d3.v3.min.js", implementChart(adjustData(parsedFloats)));
             }
         }
     }
@@ -148,29 +160,157 @@ getQueryResponseAndSetChart(query);
 
 
 //Implementation function of the graph
-var implementChart = function(valuesArray) {
+  var implementChart = function(valuesArray) {
 
-    var chart = c3.generate({
-        bindto: ".right-dock.open",
-    size: {
-            height: 500,
-            width: 300
-        },
+var data = [ { label: "Data Set 1", 
+               x: valuesArray[0], 
+               y: valuesArray[1] } ] ;
+var xy_chart = d3_xy_chart()
+    .width(660)
+    .height(350)
+    .xlabel("X Axis")
+    .ylabel("Y Axis") ;
+var svg = d3.select(".right-dock.open").append("svg")
+    .datum(data)
+    .call(xy_chart) ;
 
-    zoom: {
-        enabled: true
-     },
+function d3_xy_chart() {
+    var width = 440,  
+        height = 280, 
+        xlabel = "X Axis Label",
+        ylabel = "Y Axis Label" ;
+    
+    function chart(selection) {
+        selection.each(function(datasets) {
+            //
+            // Create the plot. 
+            //
+            var margin = {top: 20, right: 80, bottom: 30, left: 50}, 
+                innerwidth = width - margin.left - margin.right,
+                innerheight = height - margin.top - margin.bottom ;
+            
+            var x_scale = d3.scale.linear()
+                .range([0, innerwidth])
+                .domain([ d3.min(datasets, function(d) { return d3.min(d.x); }), 
+                          d3.max(datasets, function(d) { return d3.max(d.x); }) ]) ;
+            
+            var y_scale = d3.scale.linear()
+                .range([innerheight, 0])
+                .domain([ d3.min(datasets, function(d) { return d3.min(d.y); }),
+                          d3.max(datasets, function(d) { return d3.max(d.y); }) ]) ;
 
-    point: {
-        show: false
-        },
+            var color_scale = d3.scale.category10()
+                .domain(d3.range(datasets.length)) ;
 
-        data: {
-          columns: [
-            valuesArray
-          ]
-        }
-    });
+            var x_axis = d3.svg.axis()
+                .scale(x_scale)
+                .orient("bottom") ;
+
+            var y_axis = d3.svg.axis()
+                .scale(y_scale)
+                .orient("left") ;
+
+            var x_grid = d3.svg.axis()
+                .scale(x_scale)
+                .orient("bottom")
+                .tickSize(-innerheight)
+                .tickFormat("") ;
+
+            var y_grid = d3.svg.axis()
+                .scale(y_scale)
+                .orient("left") 
+                .tickSize(-innerwidth)
+                .tickFormat("") ;
+
+            var draw_line = d3.svg.line()
+                .interpolate("basis")
+                .x(function(d) { return x_scale(d[0]); })
+                .y(function(d) { return y_scale(d[1]); }) ;
+
+            var svg = d3.select(this)
+                .attr("width", width)
+                .attr("height", height)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")") ;
+            
+            svg.append("g")
+                .attr("class", "x grid")
+                .attr("transform", "translate(0," + innerheight + ")")
+                .call(x_grid) ;
+
+            svg.append("g")
+                .attr("class", "y grid")
+                .call(y_grid) ;
+
+            svg.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + innerheight + ")") 
+                .call(x_axis)
+                .append("text")
+                .attr("dy", "-.71em")
+                .attr("x", innerwidth)
+                .style("text-anchor", "end")
+                .text(xlabel) ;
+            
+            svg.append("g")
+                .attr("class", "y axis")
+                .call(y_axis)
+                .append("text")
+                .attr("transform", "rotate(-90)")
+                .attr("y", 6)
+                .attr("dy", "0.71em")
+                .style("text-anchor", "end")
+                .text(ylabel) ;
+
+            var data_lines = svg.selectAll(".d3_xy_chart_line")
+                .data(datasets.map(function(d) {return d3.zip(d.x, d.y);}))
+                .enter().append("g")
+                .attr("class", "d3_xy_chart_line") ;
+            
+            data_lines.append("path")
+                .attr("class", "line")
+                .attr("d", function(d) {return draw_line(d); })
+                .attr("stroke", function(_, i) {return color_scale(i);}) ;
+            
+            data_lines.append("text")
+                .datum(function(d, i) { return {name: datasets[i].label, final: d[d.length-1]}; }) 
+                .attr("transform", function(d) { 
+                    return ( "translate(" + x_scale(d.final[0]) + "," + 
+                             y_scale(d.final[1]) + ")" ) ; })
+                .attr("x", 3)
+                .attr("dy", ".35em")
+                .attr("fill", function(_, i) { return color_scale(i); })
+                .text(function(d) { return d.name; }) ;
+
+        }) ;
+    }
+
+    chart.width = function(value) {
+        if (!arguments.length) return width;
+        width = value;
+        return chart;
+    };
+
+    chart.height = function(value) {
+        if (!arguments.length) return height;
+        height = value;
+        return chart;
+    };
+
+    chart.xlabel = function(value) {
+        if(!arguments.length) return xlabel ;
+        xlabel = value ;
+        return chart ;
+    } ;
+
+    chart.ylabel = function(value) {
+        if(!arguments.length) return ylabel ;
+        ylabel = value ;
+        return chart ;
+    } ;
+
+    return chart;
+  }
 }
 
 // function for loading the graph library //d3js.org/d3.v3.min.js located in the index.html
@@ -205,7 +345,6 @@ function parseFloats(input) {
         helpString = helpString.slice(helpString.indexOf(" ") + 1, helpString.length);
         var parsedFloat = parseFloat(helpString.slice(0, helpString.indexOf(" ")));
     }
-    console.log(floatsArray);
     return floatsArray;
 }
 
