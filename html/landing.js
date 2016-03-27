f2eebd47e34e7e5a99d7156f74ec2264fabc8ea3
RamiroMF <r.marcofiguera@jacobs-university.de>
2016-03-27 11:30:45 +0200

    overwrite new graph

--- html/landing.js
+++ html/landing.js
@@ -218,7 +218,7 @@ var implementChart = function(floatsArray) {
     //************************************************************
     // Data notice the structure
     //************************************************************
-
+d3.select("svg").remove();
     var data = [];
     var i = 0;
     var j = 0;
@@ -242,7 +242,7 @@ var implementChart = function(floatsArray) {
     'yellow',
     'red',
     'green',
-    'orange', 
+    'orange',
     'white'
     ]
 
@@ -253,56 +253,56 @@ var implementChart = function(floatsArray) {
         width = 620 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
-    var innerwidth = width - margin.left - margin.right, 
+    var innerwidth = width - margin.left - margin.right,
         innerheight = height - margin.top - margin.bottom ;
-        
+
     var x = d3.scale.linear()
          .domain([0, floatsArray.length])
         .range([0, width]);
-     
+
     var y = d3.scale.linear()
-        .domain([0, 1])
+        .domain([0, 0.4])
         .range([height, 0]);
-        
+
     var xAxis = d3.svg.axis()
         .scale(x)
         .tickSize(-height)
-        .tickPadding(10)    
-        .tickSubdivide(true)    
-        .orient("bottom");  
-        
+        .tickPadding(10)
+        .tickSubdivide(true)
+        .orient("bottom");
+
     var yAxis = d3.svg.axis()
         .scale(y)
         .tickPadding(10)
         .tickSize(-width)
-        .tickSubdivide(true)    
+        .tickSubdivide(true)
         .orient("left");
-        
+
     var zoom = d3.behavior.zoom()
         .x(x)
         .y(y)
         .scaleExtent([1, (floatsArray.length / 4)])
-        .on("zoom", zoomed);    
-        
+        .on("zoom", zoomed);
+
     //************************************************************
     // Generate our SVG object
-    //************************************************************  
+    //************************************************************
     var svg = d3.select(".right-dock.open").append("svg")
         .call(zoom)
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-     
+
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
-     
+
     svg.append("g")
         .attr("class", "y axis")
         .call(yAxis);
-     
+
     svg.append("g")
         .attr("class", "y axis")
         .append("text")
@@ -310,33 +310,33 @@ var implementChart = function(floatsArray) {
         .attr("transform", "rotate(-90)")
         .attr("y", (-margin.left) + 10)
         .attr("x", -height/2)
-        .text('Axis Label');    
-     
+        .text('Axis Label');
+
     svg.append("clipPath")
         .attr("id", "clip")
         .append("rect")
         .attr("width", width)
         .attr("height", height);
-        
+
     //************************************************************
     // Create D3 line object and draw data on our SVG object
     //************************************************************
     var line = d3.svg.line()
-        .interpolate("linear")  
+        .interpolate("linear")
         .x(function(d) { return x(d.x); })
-        .y(function(d) { return y(d.y); });     
-        
+        .y(function(d) { return y(d.y); });
+
     svg.selectAll('.line')
         .data(data)
         .enter()
         .append("path")
         .attr("class", "line")
         .attr("clip-path", "url(#clip)")
-        .attr('stroke', function(d,i){         
+        .attr('stroke', function(d,i){
             return colors[i%colors.length];
         })
         .attr("d", line);
-        
+
     //************************************************************
     // Draw points on SVG object based on the data given
     //************************************************************
@@ -345,8 +345,8 @@ var implementChart = function(floatsArray) {
         .enter()
         .append("g")
         .attr("class", "dots")
-        .attr("clip-path", "url(#clip)");   
-     
+        .attr("clip-path", "url(#clip)");
+
       points.selectAll('.dot')
         .data(function(d, index){
             var a = [];
@@ -358,24 +358,24 @@ var implementChart = function(floatsArray) {
         .append('circle')
         .attr('class','dot')
         .attr("r", 2.5)
-        .attr('fill', function(d,i){    
+        .attr('fill', function(d,i){
             return colors[d.index%colors.length];
-        })  
-        .attr("transform", function(d) { 
+        })
+        .attr("transform", function(d) {
             return "translate(" + x(d.point.x) + "," + y(d.point.y) + ")"; }
         );
-    
+
     //************************************************************
     // Zoom specific updates
     //************************************************************
     function zoomed() {
         svg.select(".x.axis").call(xAxis);
-        svg.select(".y.axis").call(yAxis);   
-        svg.selectAll('path.line').attr('d', line);  
-     
-        points.selectAll('circle').attr("transform", function(d) { 
+        svg.select(".y.axis").call(yAxis);
+        svg.selectAll('path.line').attr('d', line);
+
+        points.selectAll('circle').attr("transform", function(d) {
             return "translate(" + x(d.point.x) + "," + y(d.point.y) + ")"; }
-        );  
+        );
     }
 }
 
