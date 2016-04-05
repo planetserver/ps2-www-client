38f5ea3645fda55ec909c866a8781aba3f71d853
RamiroMF <r.marcofiguera@jacobs-university.de>
2016-04-05 10:36:28 +0200

    update info tab

--- html/service.js
+++ html/service.js
@@ -26,9 +26,9 @@ $(function () {
             /* info dock (required) */
             var infoDock = $("<div>").infoDock().infoDock("instance");
             var infoPanel = infoDock.getInfoPanel()
-                .addTab("about", "about", "Title1", "SubTitle1", "jndfabv adfndfvbs dfasvikjadsfv")
-                .addTab("contact", "contact", "Title2", "SubTitle2", "afhofbon fbojirwegi0 fbh0bfeno")
-                .addTab("tour", "tour", "Title3", "SubTitle3", "dfabhijdfb fbohidfb dfboihbfd hoibdf ");
+                .addTab("about", "about", "PlanetServer-2", "", "PlanetServer is a Service component of the EC-funded EarthServer-2 project (#654367) aimed at serving and analyzing planetary data online, mainly through OGC Web Coverage Processing Service (WCPS). The focus of PlanetServer is on complex data, particularly hyperspectral imaging and topographic ones. Data from Mars, the Moon and other Solar System Bodies will be available and queriable.")
+                .addTab("contact", "contact", "Contact information", "", "<p>Project Manager - <a href=mailto:an.rossi@jacobs-university.de>Dr.Angelo Pio Rossi</a></p><p>Lead Developer - <a href=mailto:r.marcofiguera@jacobs-univesity.de>Ramiro Marco Figuera</a></p>")
+                .addTab("tour", "tour", "Tour", "", "Coming soon ");
 
             /* gis toolbar (required) */
             var gisToolbar = $("<div>").gisToolbar().gisToolbar("instance");
