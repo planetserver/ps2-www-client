477325bcd674afba1ae6cb58470c02688d91de7c
Bang Pham Huu <bphamhuu@jacobs-university.de>
2016-04-28 22:37:07 +0300

    change to amchart and update the infor about project

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # ps2-www-client
 
-This is PlanetServer-2
+PlanetServer 2 - http://planetserver.eu/
+------------------------
+
+Planetary Science Data Service
+
+PlanetServer is a Service component of the EC-funded EarthServer-2 project (#654367) aimed at serving and analyzing planetary data online, mainly through OGC Web Coverage Processing Service (WCPS). The focus of PlanetServer is on complex data, particularly hyperspectral imaging and topographic ones. Data from Mars, the Moon and other Solar System Bodies will be available and queriable.
+
+Ingested datasets derive from NASA Planetary Data System (PDS) and ESA Planetary Science Archive (PSA). User interest and community feedback served as input and driver to PlanetServer features and capabilities. Moreover, PlanetServer aims at supportng collaborative data analysis, being able to share planetary data hosted on a database server and querying them from a web client through any supported web browser (Chrome is advised). Please find below all relevant project-wide and service-wide URLs.
+
+EarthServer targets Earth Science domains through a Big Data Analytics approach, based on the open standards of OGC and W3C.
+
+Service access
+-------------------------
+
+PlanetServer new 3D Nasa WorldWind-powered client is available on access.planetserver.eu. The legacy EarthServer-1 client is still accessible.
+
+EarthServer targets Earth Science domains through a Big Data Analytics approach, based on the open standards of OGC and W3C.
+
+PlanetServer targets Planetary Science data and community. Online data discovery and analysis is provided through its web services/clients.
+
+Documentation
+------------------------
+
+A discussion group including developers and partners, power users of PlanetServer and EarthServer is available as Google Group.
+
+Webinars will be available on the EarthServer webinar page in the course of the project.
+
+Legacy documentation is available on the FP7 PlanetServer blog.
+
+Contacts
+------------------------
+
+The main point of contact is Dr. Angelo Pio Rossi, Service Lead and EarhtServer Coordinator.
+
+Lead developer: Ramiro Marco Figuera.
+
+Developer: Bang Pham Huu, Tsvetan Rankov.
+
+PlanetServer alumni, friends and past collaborators: Jelmer Oosthoek, Federico Cantini, Dominik Kundel, Valentine Chiwome, Software Engineering Italia.
+
--- html/modules/charts/line-chart.js
+++ html/modules/charts/line-chart.js
@@ -6,284 +6,135 @@ function getQueryResponseAndSetChart(query) {
     	success: function(data) {
                 var parsedFloats = [];
                 parsedFloats = parseFloats(data);
-                loadScriptAndCall("", implementChart(parsedFloats));
-            }
+                implementChart(parsedFloats);
+            }        
     });
 }
 
 
 //Implementation function of the graph
-    var implementChart = function(floatsArray) {
+function implementChart(floatsArray) {
 
-        //************************************************************
-        // Data notice the structure
-        //************************************************************
-        d3.select("svg").remove();
-        var data = [];
-        var i = 0,
-            j = 0;
         var xDist = 3.0 / floatsArray.length; // Value for setting the equidistance Band wavelength which should be between 1 and 4
         var xPrev = 1.0; // Value used for storing the Band wavelength of the previous Band
         var Ymin = Infinity,
             Ymax = -Infinity; // Values for getting the minimum and maximum out of the array
-        /* Adjusting the data so that every single point has a format {'x':__,'y':__} */
-        /* Splitting the datasets when 65535 is occured so that points with values 65535 are not ploted*/
-        while (i < floatsArray.length) {
-            if (floatsArray[i] != 65535) {
-                data.push([]);
-                while (floatsArray[i] != 65535) {
-                    data[j].push({
-                        'x': xPrev,
-                        'y': floatsArray[i]
-                    });
-                    if (Ymin > floatsArray[i]) { // Getting the minimum
-                        Ymin = floatsArray[i];
-                    } else if (Ymax < floatsArray[i]) { // Getting up the minimum
-                        Ymax = floatsArray[i];
-                    }
-                    xPrev = xPrev + xDist; // Setting the correct X-axis wavelength of the current Band/point
-                    i++;
-                }
-                j++;
+
+        var spectraDataProviderChart = [];
+
+
+        function SpectraDataConstructor(bandIndex, bandValue) {
+            this.bandIndex = bandIndex;
+            this.value = bandValue;
+        }
+
+        for (var i = 0; i < floatsArray.length; i++) {
+            // Only get points with valid value
+            var relectance = 0;
+
+            if (floatsArray[i] != 65535 && floatsArray[i] != 0) {
+                relectance = floatsArray[i];
+            } else {
+                relectance = null;
             }
+
+            var spectraObj = new SpectraDataConstructor(parseFloat(xPrev).toFixed(3), relectance);
+            spectraDataProviderChart.push(spectraObj);
+
+            if (Ymin > floatsArray[i]) { // Getting the minimum of value to draw chart
+                Ymin = floatsArray[i];
+            } else if (Ymax < floatsArray[i]) { // Getting the maximum of value to draw chart
+                Ymax = floatsArray[i];
+            }
+
+            // If point value is valid or not valid still need to calculate the X coordinate for it.
             xPrev = xPrev + xDist; // Setting the correct X-axis wavelength of the current Band/point
-            i++;
         }
-
-        /*Different collors for plotting the distinct datasets formed in the above while loop*/
-        var colors = ['white'];
-
-        var formatValue = d3.format(",.4f"); // Function to approximate a value
-        var bisectXval = d3.bisector(function(d) {
-            return d.x;
-        }).left;
-
-        //************************************************************
-        // Create Margins and Axis and hook our zoom function
-        //************************************************************
-        var margin = {
-                top: 0,
-                right: 20,
-                bottom: 40,
-                left: 60
+     
+       // Only add chart div when it does not exist
+       if(!$("#chartdiv").length) {
+       	    $("#mCSB_3_container").append("<div class='chartdiv' id='chartdiv' style='width:100%; height:560px;'></div>");
+       }
+       var chart = AmCharts.makeChart(".chartdiv", {
+            "type": "serial",
+            "theme": "light",
+            "marginRight": 10,
+	    "marginLeft": 10,
+            "autoMarginOffset": 20,
+            "mouseWheelZoomEnabled": true,
+            "dataProvider": spectraDataProviderChart,
+	    "fontSize": 14,
+	    "color": "#e6e600",
+            "valueAxes": [{
+                "axisAlpha": 0,
+                "guides": [{
+                    "fillAlpha": 0.1,
+                    "fillColor": "#888888",
+                    "lineAlpha": 0,
+                    "toValue": 16,
+                    "value": 10
+                }],
+                "position": "left",
+                "tickLength": 0,
+                "title": "Reflectance"
+            }],
+            "categoryAxis": {
+                "title": "Wavelength"
             },
-            width = 620 - margin.left - margin.right,
-            height = 310 - margin.top - margin.bottom;
+            "graphs": [{
+                "id": "g1",
+                "balloonText": "<span style='font-size:14px; color: #ff0000'> Wave Length:[[bandIndex]]<br><b> Reflectance:[[value]]</span></b>",
+                "bullet": "round",
+                "dashLength": 3,
+                "colorField": "color",
+                "valueField": "value",
+                "title": "Wavelength",
+                "connect": false,
+            }],
 
-        var innerwidth = width - margin.left - margin.right,
-            innerheight = height - margin.top - margin.bottom;
-
-        var x = d3.scale.linear()
-            .domain([1.0, 4.0])
-            .range([0, width]);
-
-        var y = d3.scale.linear()
-            .domain([Ymin, Ymax])
-            .range([height, 0]);
-
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            .tickSize(-height)
-            .tickPadding(10)
-            .tickSubdivide(true)
-            .orient("bottom");
-
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .tickSize(-width)
-            .tickPadding(10)
-            .tickSubdivide(true)
-            .orient("left");
-
-        var zoom = d3.behavior.zoom()
-            .x(x)
-            .y(y)
-            .scaleExtent([0.5, (floatsArray.length / 4)]) // 1st value is for zooming out; 2nd is for zooming in
-            .on("zoom", zoomed);
-
-        //************************************************************
-        // Generate our SVG object
-        //************************************************************
-        // $('<img>', {id: "ts_image"}).appendTo("#mCSB_3_container")
-        var svg = d3.select("#mCSB_3_container").append("svg")
-            .call(zoom)
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        svg.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
-            .call(xAxis);
-
-        svg.append("g")
-            .attr("class", "y axis")
-            // .attr("transform", "translate(0," + width + ")")
-            .call(yAxis);
-
-        svg.append("g")
-            .attr("class", "y axis")
-            .append("text")
-            .attr("class", "axis-label")
-            .attr("transform", "rotate(-90)")
-            .attr("y", (-margin.left) + 10)
-            .attr("x", -height / 2)
-            .style("text-anchor", "end")
-            .text('Reflectance');
-
-        svg.append("g")
-            .attr("class", "x axis")
-            .append("text")
-            .attr("class", "axis-label")
-            // .attr("transform", "rotate(-90)")
-            .attr("y", 305)
-            .attr("x", 235)
-            .text('Wavelength (μm)');
-
-        svg.append("clipPath")
-            .attr("id", "clip")
-            .append("rect")
-            .attr("width", width)
-            .attr("height", height);
-
-
-        //************************************************************
-        // Create D3 line object and draw data on our SVG object
-        //************************************************************
-        var line = d3.svg.line()
-            .interpolate("linear")
-            .x(function(d) {
-                return x(d.x);
-            })
-            .y(function(d) {
-                return y(d.y);
-            });
-
-        svg.selectAll('.line')
-            .data(data)
-            .enter()
-            .append("path")
-            .attr("class", "line")
-            .attr("clip-path", "url(#clip)")
-            .attr('stroke', function(d, i) {
-                return colors[i % colors.length];
-            })
-            .attr("d", line);
-
-        var focus = svg.append("g")
-            .attr("class", "focus")
-            .style("display", "none");
-
-        focus.append("circle")
-            .attr("r", 4.5);
-
-        focus.append("text")
-            .attr("x", 20)
-            .attr("dy", ".35em");
-
-        svg.append("rect")
-            .attr("class", "overlay")
-            .attr("width", width)
-            .attr("height", height)
-            .on("mouseover", function() {
-                focus.style("display", null);
-            })
-            .on("mouseout", function() {
-                focus.style("display", "none");
-            })
-            .on("mousemove", mousemove);
-
-        //************************************************************
-        // Draw points on SVG object based on the data given
-        //************************************************************
-        var points = svg.selectAll('.dots')
-            .data(data)
-            .enter()
-            .append("g")
-            .attr("class", "dots")
-            .attr("clip-path", "url(#clip)");
-
-        points.selectAll('.dot')
-            .data(function(d, index) {
-                var a = [];
-                d.forEach(function(point, i) {
-                    a.push({
-                        'index': index,
-                        'point': point
-                    });
-                });
-            })
-            .enter()
-            .append('circle')
-            .attr('class', 'dot')
-            .attr("r", 2.5)
-            .attr('fill', function(d, i) {
-                return colors[d.index % colors.length];
-            })
-            .attr("transform", function(d) {
-                return "translate(" + x(d.point.x) + "," + y(d.point.y) + ")";
-            });
-
-        //************************************************************
-        // Zoom specific updates
-        //************************************************************
-        function zoomed() {
-            svg.select(".x.axis").call(xAxis);
-            svg.select(".y.axis").call(yAxis);
-            svg.selectAll('path.line').attr('d', line);
-
-            points.selectAll('circle').attr("transform", function(d) {
-                return "translate(" + x(d.point.x) + "," + y(d.point.y) + ")";
-            });
-        }
-
-        function mousemove() {
-            var x0 = x.invert(d3.mouse(this)[0]); // The X value of the exact location of the mouse
-            for (var j = 0; j < data.length; j++) { // Iterating over all datasets to locate in which one is x0
-                if (x0 >= data[j][0].x && x0 <= data[j][(data[j].length) - 1].x) {
-                    var i = bisectXval(data[j], x0, 1); // index for locating point with X value close to the mouse location
-                    var d0 = data[j][i - 1];
-                    var d1 = data[j][i];
-                    var d = x0 - d0.x > d1.x - x0 ? d1 : d0; // d is a point to be shown on the graph
-                    focus.attr("transform", "translate(" + x(d.x) + "," + y(d.y) + ")");
-                    focus.select("text").text("Wavelength: " + formatValue(d.x) + " Reflectance: " + formatValue(d.y));
-                }
+            "chartCursor": {
+                "pan": true,
+                "valueLineEnabled": true,
+                "valueLineBalloonEnabled": true,
+                "cursorAlpha": 1,
+                "cursorColor": "#258cbb",
+                "limitToGraph": "g1",
+                "valueLineAlpha": 0.2,
+                "valueZoomable": true
+            },
+            "categoryField": "bandIndex",
+            "categoryAxis": {
+                //"parseDates": true,
+                "axisAlpha": 0,
+                "gridAlpha": 0.1,
+                "minorGridAlpha": 0.1,
+                "minorGridEnabled": true,
+                "title": "Wavelength"
+            },
+            "export": {
+                "enabled": true
             }
-        }
+        });
+
+	// it needs to resize the chart and write it when it is hidden	
+      	chart.invalidateSize();
+	chart.write('chartdiv');
+
+	$(".export-main").css("margin-top","-30px");
+}
+
+// Parse float from string of float values in CSV ('{"0.2323 0.342 0.436"}')
+function parseFloats(input) {
+    var floatsArray = [];
+    input = input.match(/"([^"]+)"/)[1];
+    floatsArray = input.split(" ");
+
+    // convert string value to float
+    for(var i = 0; i < floatsArray.length; i++) {
+	floatsArray[i] = parseFloat(floatsArray[i]);
     }
-
-    // function for loading the graph library //d3js.org/d3.v3.min.js located in the index.html
-    var serverResponse = " ";
-
-    function loadScriptAndCall(url, callback) {
-        // Adding the script tag to the head as suggested before
-        var head = document.getElementsByTagName('head')[0];
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.src = url;
-
-        // Then bind the event to the callback function.
-        // There are several events for cross browser compatibility.
-        script.onreadystatechange = callback;
-        script.onload = callback;
-
-        // Fire the loading
-        head.appendChild(script);
-    }
-
-
-    function parseFloats(input) {
-        var floatsArray = [];
-        var helpString = input;
-
-        var parsedFloat = parseFloat(helpString.slice(2, helpString.indexOf(" ")));
-
-        while (helpString.indexOf(" ") != -1) {
-            floatsArray.push(parsedFloat);
-            helpString = helpString.slice(helpString.indexOf(" ") + 1, helpString.length);
-            var parsedFloat = parseFloat(helpString.slice(0, helpString.indexOf(" ")));
-        }
-
-        return floatsArray;
-    }
+ 
+    //console.log("after filter null values:");
+    //console.log(floatsArray);
+    return floatsArray;
+}
