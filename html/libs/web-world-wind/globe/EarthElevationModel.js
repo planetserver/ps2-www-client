865c0e5c0595718ac1c51b6b111f88df3ce9127c
RamiroMF <r.marcofiguera@jacobs-university.de>
2016-07-27 13:28:03 +0200

    create moon dtm

--- html/libs/web-world-wind/globe/EarthElevationModel.js
+++ html/libs/web-world-wind/globe/EarthElevationModel.js
@@ -29,18 +29,35 @@ define([
             ElevationModel.call(this,
                 Sector.FULL_SPHERE, new Location(45, 45), 12, "image/bil", "tiles_test", 256, 256);
 
-            this.displayName = "Earth Elevation Model";
-            this.minElevation = -11000; // Depth of Marianas Trench, in meters
-            this.maxElevation = 8850; // Height of Mt. Everest
-            this.pixelIsPoint = false; // World Wind WMS elevation layers return pixel-as-area images
+                if (url.indexOf("moon") > -1){
+                  this.displayName = "Moon Elevation Model";
+                  this.minElevation = -9115; // Depth of Marianas Trench, in meters
+                  this.maxElevation = 10786; // Height of Mt. Everest
+                  this.pixelIsPoint = false; // World Wind WMS elevation layers return pixel-as-area images
 
-            /*this.urlBuilder = new WmsUrlBuilder("http://worldwind26.arc.nasa.gov/elev1",
-                                "GEBCO,aster_v2,USGS-NED", "", "1.3.0");*/
-            //var testEndPoint = "http://access.planetserver.eu:8080/geoserver/WWW_DEM_10km_1file/wms";
-            //var layer = "WWW_DEM_10km_1file:tiles_test";
-            var testEndPoint = "http://access.planetserver.eu:8080/geoserver/global_dem/wms";
-            var layer = "global_dem:tiles_cut";
-            this.urlBuilder = new WmsUrlBuilder(testEndPoint, layer, "", "1.1.0");
+                  /*this.urlBuilder = new WmsUrlBuilder("http://worldwind26.arc.nasa.gov/elev1",
+                                      "GEBCO,aster_v2,USGS-NED", "", "1.3.0");*/
+                  //var testEndPoint = "http://access.planetserver.eu:8080/geoserver/WWW_DEM_10km_1file/wms";
+                  //var layer = "WWW_DEM_10km_1file:tiles_test";
+                  var testEndPoint = "http://access.planetserver.eu:8080/geoserver/lunar_global/wms";
+                  var layer = "lunar_global:tiles_cut";
+                  this.urlBuilder = new WmsUrlBuilder(testEndPoint, layer, "", "1.1.0");
+
+                } else{
+                  this.displayName = "Mars Elevation Model";
+                  this.minElevation = -8201; // Depth of Marianas Trench, in meters
+                  this.maxElevation = 21241; // Height of Mt. Everest
+                  this.pixelIsPoint = false; // World Wind WMS elevation layers return pixel-as-area images
+
+                  /*this.urlBuilder = new WmsUrlBuilder("http://worldwind26.arc.nasa.gov/elev1",
+                                      "GEBCO,aster_v2,USGS-NED", "", "1.3.0");*/
+                  //var testEndPoint = "http://access.planetserver.eu:8080/geoserver/WWW_DEM_10km_1file/wms";
+                  //var layer = "WWW_DEM_10km_1file:tiles_test";
+                  var testEndPoint = "http://access.planetserver.eu:8080/geoserver/global_dem/wms";
+                  var layer = "global_dem:tiles_cut";
+                  this.urlBuilder = new WmsUrlBuilder(testEndPoint, layer, "", "1.1.0");
+                }
+
         };
 
         EarthElevationModel.prototype = Object.create(ElevationModel.prototype);
