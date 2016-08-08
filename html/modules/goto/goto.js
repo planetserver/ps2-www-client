df9ca281eda2563b4e40a6a673eafc046caec709
Bang Pham Huu <bphamhuu@jacobs-university.de>
2016-08-08 21:15:30 +0300

    combine goto functions to 1

--- html/modules/goto/goto.js
+++ html/modules/goto/goto.js
@@ -1,8 +1,9 @@
-$( document ).ready(function() {
+$(document).ready(function() {
 
     // All regions are possible
     var availableRegionsJSON = [];
     var availableRegions = [];
+    var selectedValue = "region";
 
     $.ajax({
         type: "GET",
@@ -12,7 +13,7 @@ $( document ).ready(function() {
         async: false,
         success: function(data) {
             availableRegionsJSON = data;
-            for(var i = 0; i < availableRegionsJSON.length; i++) {
+            for (var i = 0; i < availableRegionsJSON.length; i++) {
                 availableRegions.push(availableRegionsJSON[i].name);
             }
 
@@ -20,102 +21,163 @@ $( document ).ready(function() {
         }
     });
 
+    // default load auto suggestion for region
+    $(function() {
+        loadAutoCompleteRegion();
+    });
+
+
+    // Handle event when select dropdownGoto
+    $("#dropdownGoto").change(function () {
+        // get the option value
+        var value = this.value;
+        if (value === "region") {
+            // load the autocomple text on the text box for "region"
+            loadAutoCompleteRegion();
+            selectedValue = "region";
+        } else if (value === "coverageID") {
+            // load the autocomple text on the text box for "coverageID"
+            loadAutoCompleteCoverageID();
+            selectedValue = "coverageID";
+        } else {
+            // lat,long goto
+            loadAutoCompleteLatLong();
+            selectedValue = "latlon";
+        }
+    });
+
 
     // All footprints are possible
     var availableCoverageIDs = [];
-    for(var i = 0; i < allFootPrintsArray.length; i++){
+    for (var i = 0; i < allFootPrintsArray.length; i++) {
         availableCoverageIDs.push(allFootPrintsArray[i].coverageID);
     }
 
     // Get the range when scroll
-    $('body').bind('mousewheel', function(e){
-        if(e.originalEvent.wheelDelta /120 > 0) {
+    $('body').bind('mousewheel', function(e) {
+        if (e.originalEvent.wheelDelta / 120 > 0) {
             updateRange();
-        }
-        else{
+        } else {
             updateRange();
         }
     });
 
 
-    /* add goto regions search */
-    $( ".autocompleteRegionGoTo" ).autocomplete({
-        // Only get top 10 result for faster response
-        minLength: 0, // need to show regions when focus
-        source: function(request, response) {
-            var results = $.ui.autocomplete.filter(availableRegions, request.term);
-            response(results.slice(0, 10));
-        }
-    }).focus(function () {
-        // Get all regions when focus on
-        $(this).autocomplete("search", "");
-    });
+    function loadAutoCompleteRegion() {
+        /* add goto regions search */
+        $("#txtGoto").autocomplete({
+            // Only get top 10 result for faster response
+            minLength: 0, // need to show regions when focus
+            source: function(request, response) {
+                var results = $.ui.autocomplete.filter(availableRegions, request.term);
+                response(results.slice(0, 10));
+            }
+        }).focus(function() {
+            // Get all regions when focus on
+            $(this).autocomplete("search", "");
+        });
 
-    /* add band WCPS suggestion in RGB bands combination */
-    $( ".autocompleteCoverageIDGoTo" ).autocomplete({
-        // Only get top 10 result for faster response
-        source: function(request, response) {
-            var results = $.ui.autocomplete.filter(availableCoverageIDs, request.term);
-            response(results.slice(0, 10));
+        $("#txtGoto").val("");
+    }
+
+    function loadAutoCompleteCoverageID() {
+        /* add coverageID suggestion */
+        $("#txtGoto").autocomplete({
+            // Only get top 10 result for faster response
+            source: function(request, response) {
+                var results = $.ui.autocomplete.filter(availableCoverageIDs, request.term);
+                response(results.slice(0, 10));
+            }
+        });
+
+        $("#txtGoto").val("");
+    }
+
+    function loadAutoCompleteLatLong() {
+        $("#txtGoto").autocomplete({
+            // clear the auto complete on text box
+            source: function(request, response) {
+                var results = "";
+                response(results);
+            }
+        });
+
+        $("#txtGoto").val("");
+    }
+
+
+    // Handle button goto 
+    $("#btnGoto").on("click", function() {
+        if (selectedValue === "region") {
+            // goto region
+            gotoRegion();
+        } else if (selectedValue === "coverageID") {
+            // goto coverageID
+            gotoCoverageID();
+        } else {
+            // goto latlong
+            gotoLatLon();
         }
     });
 
 
     // goto region to allow goto coverageID or (Latitude and Longitude)
-    $("#btnRegionGoTo").on("click", function() {
-        var region = $("#txtRegionGoTo").val();
+    function gotoRegion() {
+        var region = $("#txtGoto").val();
         // User want to go to region
-        if(region !== "") {
-            for(var i = 0; i < availableRegionsJSON.length; i++){
-                if(availableRegionsJSON[i].name == region) {
+        if (region !== "") {
+            for (var i = 0; i < availableRegionsJSON.length; i++) {
+                if (availableRegionsJSON[i].name == region) {
                     // Move to region
                     moveToFootPrint(availableRegionsJSON[i].latitude, availableRegionsJSON[i].longitude);
+                    makeLinkGoTo("", availableRegionsJSON[i].latitude, availableRegionsJSON[i].longitude);
                     return true;
                 }
             }
-
-            alert("You need to choose existing region on globe.");
-            return false;
         }
-    });
+        alert("You need to choose existing region on globe.");
+        return false;
+    }
 
-    // goto CoverageID to allow goto Latitude and Longitude
-    $("#btnCoverageIDGoTo").on("click", function() {
-      var coverageID = $("#txtCoverageIDGoTo").val();
+    // goto CoverageID to allow goto CoverageID
+    function gotoCoverageID() {
+        var coverageID = $("#txtGoto").val();
 
-        if(coverageID !== "") {
+        if (coverageID !== "") {
             moveToCoverageID(coverageID);
 
             makeLinkGoTo(coverageID, "", "");
-        } else {
-            alert("Please type existing coverage name on globe.");
-            return false;
+            return true;
         }
-    });
+        alert("Please type existing coverage name on globe.");
+        return false;
+    }
 
-    // goto Latitude, Longitude to allow goto Latitude and Longitude
-    $("#btnLatLonGoTo").on("click", function() {
-        var latitude = $("#txtLatitudeGoTo").val();
-        var longitude = $("#txtLongitudeGoTo").val();
+    // goto Latitude, Longitude to allow goto Latitude and Longitude (lat,lon)
+    function gotoLatLon() {
+        var latlon = $("#txtGoto").val().split(",");
+        var latitude = latlon[0];
+        var longitude = latlon[1];
 
-        if(checkLatitude(latitude) && checkLongitude(longitude)) {
+        if (checkLatitude(latitude) && checkLongitude(longitude)) {
             moveToFootPrint(latitude, longitude);
 
             makeLinkGoTo("", latitude, longitude);
-        } else {
-            return false;
+            return true;
         }
-    });
+        alert("Please type valid Latitude,Longitude (e.g: 20.5,120.5)");
+        return false;
+    };
 
 
     /* Get the latitude, longitude of region/coverageID or manual latitude, longitude to goto */
     function makeLinkGoTo(coverageID, latitude, longitude) {
         // Generate a link to access to coverageID and/or Latitude and Longitude
         var link = ps2EndPoint + "index.html?";
-        if(coverageID != "") {
+        if (coverageID != "") {
             link = link + "covName=" + coverageID;
         }
-        if(latitude != "" && longitude != "") {
+        if (latitude != "" && longitude != "") {
             link = link + "&lat=" + latitude;
             link = link + "&lon=" + longitude;
         }
@@ -131,27 +193,27 @@ $( document ).ready(function() {
 
     // Check if latitude, longitude is valid
     function checkLatitude(latitude) {
-    	val = parseFloat(latitude);
-		if(isNaN(latitude) || latitude === "") {
-			alert("Latitude must be float value!");
-			return false;
-		} else if (latitude > 90 || latitude < -90) {
-			alert("Latitude must be between (-90:90) degree.");
-			return false;
-		}
-		return true;
+        val = parseFloat(latitude);
+        if (isNaN(latitude) || latitude === "") {
+            alert("Latitude must be float value!");
+            return false;
+        } else if (latitude > 90 || latitude < -90) {
+            alert("Latitude must be between (-90:90) degree.");
+            return false;
+        }
+        return true;
     }
 
     function checkLongitude(longitude) {
-    	val = parseFloat(longitude);
-		if(isNaN(longitude) || longitude === "") {
-			alert("Longitude must be float value!");
-			return false;
-		} else if (longitude > 180 || longitude < -180) {
-			alert("Longitude must be between (-180:180) degree.");
-			return false;
-		}
-		return true;
+        val = parseFloat(longitude);
+        if (isNaN(longitude) || longitude === "") {
+            alert("Longitude must be float value!");
+            return false;
+        } else if (longitude > 180 || longitude < -180) {
+            alert("Longitude must be between (-180:180) degree.");
+            return false;
+        }
+        return true;
     }
 
 
@@ -159,9 +221,9 @@ $( document ).ready(function() {
     function moveToCoverageID(coverageID) {
         for (var i = 0; i < allFootPrintsArray.length; i++) {
             if (coverageID.toLowerCase() === allFootPrintsArray[i].coverageID.toLowerCase()) {
-            	var centerLatitude = (allFootPrintsArray[i].Maximum_latitude + allFootPrintsArray[i].Minimum_latitude) / 2;
-            	var centerLongitude = (allFootPrintsArray[i].Easternmost_longitude + allFootPrintsArray[i].Westernmost_longitude) / 2;
-            	moveToFootPrint(centerLatitude, centerLongitude);
+                var centerLatitude = (allFootPrintsArray[i].Maximum_latitude + allFootPrintsArray[i].Minimum_latitude) / 2;
+                var centerLongitude = (allFootPrintsArray[i].Easternmost_longitude + allFootPrintsArray[i].Westernmost_longitude) / 2;
+                moveToFootPrint(centerLatitude, centerLongitude);
                 return;
             }
         }
@@ -171,16 +233,16 @@ $( document ).ready(function() {
 
     // Move to latitude, longitude coordinate
     function moveToFootPrint(latitude, longitude) {
-    	// Keep the user current zoom
-    	//wwd.navigator.range = 2e6;
-		wwd.goTo(new WorldWind.Location(latitude, longitude));
+        // Keep the user current zoom
+        //wwd.navigator.range = 2e6;
+        wwd.goTo(new WorldWind.Location(latitude, longitude));
 
-		// Put placemark, remove the last clicked point
+        // Put placemark, remove the last clicked point
         if (placemarkLayer != null) {
             wwd.removeLayer(placemarkLayer);
         }
 
-		var placemark = new WorldWind.Placemark(new WorldWind.Position(latitude, longitude, 1e2), true, null);
+        var placemark = new WorldWind.Placemark(new WorldWind.Position(latitude, longitude, 1e2), true, null);
         var placemarkAttributes = new WorldWind.PlacemarkAttributes(placemarkAttributes);
         placemarkAttributes.imageSource = "html/images/close.png";
         placemark.attributes = placemarkAttributes;
@@ -194,25 +256,25 @@ $( document ).ready(function() {
 
     // Update range in link of goto panel when scroll
     function updateRange() {
-    	var range = wwd.navigator.range;
-    	var linkText = $("#linkGoTo").text();
+        var range = wwd.navigator.range;
+        var linkText = $("#linkGoTo").text();
 
-    	//console.log(linkText);
+        //console.log(linkText);
 
-    	var tmp = "";
-    	// If user has not clicked on map and scroll
-    	if(linkText.indexOf("range") < 0) {
-    		tmp = ps2EndPoint + "index.html?range=" + range;
-    	} else {
-    		// update the new range
-	    	var startIndex = linkText.lastIndexOf("=");
+        var tmp = "";
+        // If user has not clicked on map and scroll
+        if (linkText.indexOf("range") < 0) {
+            tmp = ps2EndPoint + "index.html?range=" + range;
+        } else {
+            // update the new range
+            var startIndex = linkText.lastIndexOf("=");
 
-	    	tmp = linkText.substr(0, startIndex + 1);
-	    	tmp = tmp + range;
-    	}
+            tmp = linkText.substr(0, startIndex + 1);
+            tmp = tmp + range;
+        }
 
-    	// Set the new range to link
-    	$("#linkGoTo").text(tmp);
-    	$("#linkGoTo").attr("href", tmp);
+        // Set the new range to link
+        $("#linkGoTo").text(tmp);
+        $("#linkGoTo").attr("href", tmp);
     }
 });
--- html/template/panels/goto-panel.js
+++ html/template/panels/goto-panel.js
@@ -7,18 +7,12 @@ $(function () {
             this._super();
             this.panelBody
                 .append(
-                    $("<div id='gotoPanel' style='float: right;background-color: rgba(47, 85, 151, .2);'>"+
+                    $("<div id='gotoPanel' style='float: right;background-color: rgba(47, 85, 151, .2); '>"+
                       "<table style='border-spacing: 10px; border-collapse: separate; width: 400px; color: white;'>"+
-                        //"<tr> <td colspan='3' style='font-size:10px;'> Please choose Region or Coverage Name or (Latitude and Longitude) to navigate on globe.</td> </tr>"+
-                        "<tr> <td>Region: </td> <td colspan='2'> <input type='text' id='txtRegionGoTo' class='autocompleteRegionGoTo' style='width:81%; color:blue; font-size:12px;'/> <button id='btnRegionGoTo' type='button' class='btn btn-success' style='height: 20px; font-size: 10px;'>Goto</button> </td> </tr>"+
-                        "<tr> <td>Product Id: </td> <td colspan='2'> <input type='text' id='txtCoverageIDGoTo' class='autocompleteCoverageIDGoTo' style='width:81%; color:blue; font-size:12px;'/> <button id='btnCoverageIDGoTo' type='button' class='btn btn-warning' style='height: 20px; font-size: 10px;'>Goto</button> </td> </tr>"+
-                        "<tr> <td>Latitude: </td> <td colspan='2'> <input type='text' id='txtLatitudeGoTo' style='width:25%; color:blue; font-size:12px;'/>"+
-                             "&nbsp; Longitude: &nbsp; "+
-                                 "<input type='text' id='txtLongitudeGoTo' style='width:26%; color:blue; font-size:12px;'/> "+
-                             "<button id='btnLatLonGoTo' type='button' class='btn btn-danger' style='height: 20px; font-size: 10px;'>Goto</button>"+
-                             "</td> </tr>"+
-                        "<tr> <td colspan='3'> <a href='#' id='linkGoTo' style='color:yellow; font-size:9px;'></a></tr>"+
+			"<tr> <td> <button id='btnGoto' type='button' class='btn btn-success' style='height: 20px; font-size: 10px;'>Goto</button> </td>" + " <td colspan='2'> <input type='text' id ='txtGoto' style='color:blue; font-size:12px;'/>" +
+			 " <td style='color: blue;'> <select id='dropdownGoto'> <option value='region'>Region</option> <option value='coverageID'>Footprint ID</option> <option value='latlon'>Lat,Lon</option> </select> </td> </tr>" + 
                       "</table>"+
+		       "<a href='#' id='linkGoTo' style='color:yellow; font-size:9px;'></a>" + 
                     "</div>"
                     )
                 );
