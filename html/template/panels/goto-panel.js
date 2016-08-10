48a00e234aa357ef6c1c70282dacc61c4308b73f
RamiroMF <r.marcofiguera@jacobs-university.de>
2016-08-10 11:04:13 +0200

    fix small bug with search bar shifting

--- html/template/panels/goto-panel.js
+++ html/template/panels/goto-panel.js
@@ -7,12 +7,12 @@ $(function () {
             this._super();
             this.panelBody
                 .append(
-                    $("<div id='gotoPanel' style='float: right;background-color: rgba(47, 85, 151, .2); '>"+
+                    $("<div id='gotoPanel' style='float: background-color: rgba(47, 85, 151, .2); '>"+
                       "<table style='border-spacing: 10px; border-collapse: separate; width: 400px; color: white;'>"+
 			"<tr> <td> <button id='btnGoto' type='button' class='btn btn-success' style='height: 20px; font-size: 10px;'>Goto</button> </td>" + " <td colspan='2'> <input type='text' id ='txtGoto' style='color:blue; font-size:12px;'/>" +
-			 " <td style='color: blue;'> <select id='dropdownGoto'> <option value='region'>Region</option> <option value='coverageID'>Footprint ID</option> <option value='latlon'>Lat,Lon</option> </select> </td> </tr>" + 
+			 " <td style='color: blue;'> <select id='dropdownGoto'> <option value='region'>Region</option> <option value='coverageID'>Footprint ID</option> <option value='latlon'>Lat,Lon</option> </select> </td> </tr>" +
                       "</table>"+
-		       "<a href='#' id='linkGoTo' style='color:yellow; font-size:9px;'></a>" + 
+		       "<a href='#' id='linkGoTo' style='color:yellow; font-size:9px;'></a>" +
                     "</div>"
                     )
                 );
